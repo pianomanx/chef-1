5b87d592526d19b79cd84afdf05163a7d5f384db
Seth Kingry <sjkingry@gmail.com>
2025-10-02 13:20:28 -0400

    Adding Docker role from Galaxy

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
 apt update
 apt install -y build-essential
 apt install -y software-properties-common
 apt install -y openssh-server
+
 add-apt-repository --yes --update ppa:ansible/ansible
+
 apt install -y ansible
+
 ansible-galaxy role install arillso.sshd
+ansible-galaxy role install geerlingguy.docker
+ansible-galaxy role install geerlingguy.pip
 ansible-galaxy role install stuvusIT.systemd-timesyncd
+
 mkdir -p /run/sshd
